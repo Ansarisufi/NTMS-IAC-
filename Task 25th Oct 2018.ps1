3da5c9c40483101fafc963bf3915809d0d5d1da2
Ansarisufi <44155737+Ansarisufi@users.noreply.github.com>
2018-10-25 08:48:47 +0530

    Add files via upload

---
+++ Task 25th Oct 2018.ps1
@@ -0,0 +1,19 @@
+Task 25th Oct 2018
+
+1. Identify a cmdlet that produces a random number.
+Get-Random
+
+2. Indentify a cmdlet that displays the current date and time.
+Get-Date
+
+3. What type of object the cmdlet from task 2 produce?
+System.Datetime
+
+4.Identify the cmdlet that displays information about installed hotfixes on windows.
+Get-Hotfix
+
+5. Display a list of 50 newest entries from secjurity log.
+Get-Eventlog -Logname Security -Newest 50
+
+
+
